ac7f5992f5255d4cec81df2da9c7ecfa6a440929
artem <artemiyka95@gmail.com>
2023-01-07 08:26:09 +0300

    Initial commit

---
+++ example/pure_ftp_example.dart
@@ -0,0 +1,6 @@
+import 'package:pure_ftp/pure_ftp.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/pure_ftp.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library pure_ftp;
+
+export 'src/pure_ftp_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
