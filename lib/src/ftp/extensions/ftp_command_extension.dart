82e5acb20168c8c3fddcca2616bfa7a75ba203cc
artem <artemiyka95@gmail.com>
2023-01-09 08:20:49 +0300

    make ftp command extension more safe

--- lib/src/ftp/extensions/ftp_command_extension.dart
+++ lib/src/ftp/extensions/ftp_command_extension.dart
@@ -3,14 +3,14 @@ import 'package:pure_ftp/src/ftp/ftp_response.dart';
 import 'package:pure_ftp/src/ftp/ftp_socket.dart';
 
 extension FtpCommandExtension on FtpCommand {
-  void write(FtpSocket socket, [List<dynamic>? args]) {
+  void write(FtpSocket socket, [List<String>? args]) {
     socket.write([
       name,
       ...?args?.map((e) => e.toString()),
     ].join(' '));
   }
 
-  Future<FtpResponse> writeAndRead(FtpSocket socket, [List<dynamic>? args]) {
+  Future<FtpResponse> writeAndRead(FtpSocket socket, [List<String>? args]) {
     write(socket, args);
     return socket.read();
   }
