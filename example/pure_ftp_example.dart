631a6399669c40cc48fad57e22f30b2e18061fd1
artem <artemiyka95@gmail.com>
2023-01-11 06:12:00 +0300

    update example with the latest changes

--- example/pure_ftp_example.dart
+++ example/pure_ftp_example.dart
@@ -1,11 +1,17 @@
 import 'dart:io';
 
 import 'package:pure_ftp/pure_ftp.dart';
+import 'package:pure_ftp/src/file_system/entries/ftp_link.dart';
+import 'package:pure_ftp/src/file_system/ftp_file_system.dart';
 import 'package:pure_ftp/src/ftp/ftp_socket.dart';
 import 'package:yaml/yaml.dart';
 
 void main() async {
-  final config = loadYaml(await File('default_connection.yml').readAsString());
+  var configFile = File('test_connection.yml');
+  if (!configFile.existsSync()) {
+    configFile = File('default_connection.yml');
+  }
+  final config = loadYaml(await configFile.readAsString());
   final ftpSocket = FtpSocket(
     host: config['host'],
     port: config['port'],
@@ -22,5 +28,27 @@ void main() async {
     print('Connected');
   });
 
+  final fs = FtpFileSystem(socket: ftpSocket);
+
+  await _getDirList(fs);
+
+  try {
+    fs.listType = ListType.MLSD;
+    await _getDirList(fs);
+  } catch (e) {
+    // maybe server doesn't support MLSD
+    print(e);
+  }
+
   await ftpSocket.disconnect();
+}
+
+Future<void> _getDirList(FtpFileSystem fs) async {
+  final list = await fs.listDirectory();
+  list.forEach(print);
+  for (final entry in list) {
+    if (entry is FtpLink) {
+      print('LinkTarget: ${entry.path} -> ${await entry.linkTarget}');
+    }
+  }
 }
