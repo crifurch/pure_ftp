543165d751a0b2b9a01330b669d8def0ce7c971d
artem <artemiyka95@gmail.com>
2024-06-04 03:07:11 +0300

    fix small bug

--- lib/src/ftp/extensions/string_find_extension.dart
+++ lib/src/ftp/extensions/string_find_extension.dart
@@ -2,6 +2,13 @@ extension StringFindExtension on String {
   String find(String start, String end) {
     final startIndex = indexOf(start);
     final endIndex = lastIndexOf(end);
-    return substring(startIndex + start.length, endIndex);
+    if (startIndex == endIndex) {
+      return this;
+    }
+    try {
+      return substring(startIndex + start.length, endIndex);
+    } on Exception {
+      return this;
+    }
   }
 }
